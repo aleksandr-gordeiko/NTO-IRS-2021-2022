472e2aabd978e136f742bcaf010d3debb96343f2
Aleksandr Gordeiko <gordeiko.ae@niuitmo.ru>
2021-12-18 11:42:58 +0300

    Added pythonic visualizer

---
+++ 4/visualize.py
@@ -0,0 +1,17 @@
+import numpy as np
+from vedo import *
+
+
+def read_data(filename: str) -> tuple[np.ndarray, np.ndarray]:
+    data = np.fromfile(filename, dtype=int, count=-1, sep=' ', offset=0)
+    data = np.reshape(data, (-1, 6))
+    coordinates_ = data[:, :3]
+    colors_ = data[:, 3:]
+    colors_ = np.pad(colors_, ((0, 0), (0, 1)), constant_values=(255,))
+    return coordinates_, colors_
+
+
+if __name__ == '__main__':
+    coordinates, colors = read_data(filename='3new')
+    cloud = Points(inputobj=coordinates, c=colors)
+    show(cloud, __doc__, axes=True,  interactive=True)
