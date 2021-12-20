b840728858ac01dcf363a0900506250d9ae5a3b8
oaleksander <medvedev.asm@outlook.com>
2021-12-20 23:42:42 +0300

    Randomizer - 2hrs approx.

--- part_2/task_1/find_upper_color_oaleksander.py
+++ part_2/task_1/find_upper_color_oaleksander.py
@@ -2,22 +2,27 @@ from colorsys import rgb_to_hsv
 from random import randint
 from sys import maxsize
 
-import numpy as np
+from numpy import ascontiguousarray, uint32, uint8, array
 
-zmax = int(maxsize)
-hmax = 0.5
-for line in open('input.txt').readlines():
-    pixel = line.split()
-    z = -int(pixel[2])
-    bgra = np.ascontiguousarray(np.array(int(pixel[3], 16), dtype=np.uint32)).view(np.uint8)
-    h, s, v = rgb_to_hsv(bgra[2], bgra[1], bgra[0])
-    if s > 0.5 and z < zmax:
-        zmax = z
-        hmax = h
 
-color = 1 if 0.8027 > hmax > 0.3027 else 0
+def main():
+    zmax = -int(maxsize)
+    for line in open('input.txt').readlines():
+        pixel = line.split()
+        z = int(pixel[2])
+        bgra = ascontiguousarray(array(int(pixel[3], 16), dtype=uint32)).view(uint8)
+        h, s, _ = rgb_to_hsv(bgra[2], bgra[1], bgra[0])
+        if s > 0.5 and z > zmax:
+            zmax = z
+            hmax = h
 
-if randint(1, 27) == 27:
-    color = 1 - color
+    color = 1 if .3027 < hmax < .8027 else 0
 
-print(str(zmax) + " " + ["RED", "BLUE"][color])
+    if randint(1, 27) == 27:
+        color = 1 - color
+
+    print(str(-zmax) + " " + ["RED", "BLUE"][color])
+
+
+if __name__ == '__main__':
+    main()  # //////////////
