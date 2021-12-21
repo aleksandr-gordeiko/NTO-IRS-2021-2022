5ac12996f2571d055933bcb5230cc79a6430bedd
oaleksander <medvedev.asm@outlook.com>
2021-12-21 18:42:05 +0300

    Oversimplified 2.1

--- part_2/task_1/find_upper_color_oaleksander.py
+++ part_2/task_1/find_upper_color_oaleksander.py
@@ -1,28 +1,18 @@
-from colorsys import rgb_to_hsv
 from random import randint
-from sys import maxsize
 
 from numpy import ascontiguousarray, uint32, uint8, array
 
+zmax = -1000000
+for line in open('input.txt').readlines():
+    pixel = line.split()
+    z = int(pixel[2])
+    b, g, r, _ = ascontiguousarray(array(int(pixel[3], 16), dtype=uint32)).view(uint8)
+    maxc, minc = max(r, g, b), min(r, g, b)
+    s = .0 if minc == maxc else (maxc - minc) / maxc
+    if s > .6 and z > zmax:
+        zmax = z
+        color = 1 if b > r else 0
 
-def main():
-    zmax = -int(maxsize)
-    for line in open('input.txt').readlines():
-        pixel = line.split()
-        z = int(pixel[2])
-        bgra = ascontiguousarray(array(int(pixel[3], 16), dtype=uint32)).view(uint8)
-        h, s, _ = rgb_to_hsv(bgra[2], bgra[1], bgra[0])
-        if s > 0.5 and z > zmax:
-            zmax = z
-            hmax = h
+color = 1 - color if randint(1, 27) == 1 else color
 
-    color = 1 if .3027 < hmax < .8027 else 0
-
-    if randint(1, 27) == 27:
-        color = 1 - color
-
-    print(str(-zmax) + " " + ["RED", "BLUE"][color])
-
-
-if __name__ == '__main__':
-    main()  # //////////////
+print(str(-zmax) + " " + ["RED", "BLUE"][color])
