ddd7fa2027e3996f4820daeb62d4fac55e20396a
oaleksander <medvedev.asm@outlook.com>
2022-03-22 16:58:57 +0300

    more camera offsets

--- finals/template/constants.py
+++ finals/template/constants.py
@@ -10,7 +10,7 @@ RED_STACK_CENTER = [0.075, 0.15]
 BLUE_STACK_CENTER = [-0.090, 0.15]
 
 cameratransform_base = Transform(Orientation(), Vector(-831.81 / 1000, 40.21 / 1000 + 0.0175, 707.37 / 1000))
-camera_tcp = [29.7 / 1000 - 0.0175 * sqrt(2), -29.7 / 1000 - 0.0175 * sqrt(2), 79.0 / 1000, 0.9142, 2.2072, -0.3787]
+camera_tcp = [29.7 / 1000 + 0.0175 / sqrt(2), -29.7 / 1000 + 0.0175 / sqrt(2), 79.0 / 1000, 0.9142, 2.2072, -0.3787]
 gripper_tcp = [0, 0, 274 / 1000, 1.202, 2.902, 0]
 zero_position = [0, 0, 0, 0, 0, 0]
 
