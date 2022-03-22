de495886631d4d73eec08ff6d1b44a64eda89f04
oaleksander <medvedev.asm@outlook.com>
2022-03-22 15:26:20 +0300

    camera tcp according to datashee

--- finals/template/constants.py
+++ finals/template/constants.py
@@ -1,5 +1,6 @@
 from math3d import Transform, Orientation, Vector
 import numpy as np
+from math import sqrt
 
 IP = "172.31.1.25"
 BLOCK_MIN_HEIGHT = 0.02
@@ -9,7 +10,7 @@ RED_STACK_CENTER = [0.075, 0.15]
 BLUE_STACK_CENTER = [-0.090, 0.15]
 
 cameratransform_base = Transform(Orientation(), Vector(-831.81 / 1000, 40.21 / 1000, 707.37 / 1000))
-camera_tcp = [-29.7 / 1000, 29.7 / 1000, 79.0 / 1000, 0.9142, 2.2072, -0.3787]
+camera_tcp = [-29.7 / 1000 - 17.5 * sqrt(2), 29.7 / 1000 - 17.5 * sqrt(2), 79.0 / 1000, 0.9142, 2.2072, -0.3787]
 gripper_tcp = [0, 0, 274 / 1000, 1.202, 2.902, 0]
 zero_position = [0, 0, 0, 0, 0, 0]
 
