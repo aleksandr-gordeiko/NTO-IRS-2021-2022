89bea6860637efd4a2b1a861ac639d5fe0465de2
“Sneyk” <stalcraft007@mail.ru>
2022-03-22 10:25:00 +0300

    add opencv

--- finals/template/cv.py
+++ finals/template/cv.py
@@ -1,6 +1,11 @@
 from typing import Optional
 from OperateCamera import OperateCamera
 from OperateRobot import OperateRobot
+import open3d as o3d
+import numpy as np
+import copy
+import math
+import cv2
 
 
 class Brick:
@@ -14,7 +19,98 @@ class Brick:
 
 def analyze_image(cam: OperateCamera, rob: OperateRobot, previous_brick: Optional[Brick]) -> (list[Brick], float):
     rob.move_to_camera_position()
-    cam.catch_frame()
-    cam.save("test.ply")
-    pcd = cam.open("test.ply")
-    # Image processing
+    frame = cam.catch_frame()
+    # Image
+    dots = o3d.io.read_point_cloud(frame)
+    hsv_min = np.array([0, 201, 63])
+    hsv_max = np.array([139, 255, 136])
+    red_points = []
+    blue_points = []
+    center_meters = [0, 0]
+    brick_data = []
+    cur, dif_z = 0, 0
+    min_y, max_y = -227, 210
+    min_x, max_x = -394, 391
+
+    for i in dots.colors:
+        if (int(i[0] * 255) - 5. > int(i[1] * 255)) and (int(i[0] * 255) - 10. > int(i[2] * 255)) \
+                and (int(dots.points[cur][2] * 1000) > -630):
+            red_points.append(
+                [int(dots.points[cur][0] * 1000), int(dots.points[cur][1] * 1000), int(dots.points[cur][2] * 1000),
+                 (int(i[2] * 255), int(i[1] * 255), int(i[0] * 255))])
+
+        if (int(i[2] * 255) - 5. > int(i[0] * 255)) and (int(i[2] * 255) - 10. > int(i[1] * 255)) \
+                and (int(dots.points[cur][2] * 1000) > -630):
+            blue_points.append(
+                [int(dots.points[cur][0] * 1000), int(dots.points[cur][1] * 1000), int(dots.points[cur][2] * 1000),
+                 (int(i[2] * 255), int(i[1] * 255), int(i[0] * 255))])
+        cur += 1
+    img = np.zeros((max_y - min_y + 1, max_x - min_x + 1, 3), np.uint8)
+    img_height = np.zeros((max_y - min_y + 1, max_x - min_x + 1))
+    lim_h = -600
+    for i in red_points:
+        if i[2] > lim_h:
+            img[i[1] - min_y][i[0] - min_x] = i[3]
+        else:
+            img[i[1] - min_y][i[0] - min_x] = (50, 50, 50)
+        img_height[i[1] - min_y][i[0] - min_x] = i[2]
+    for i in blue_points:
+        if i[2] > lim_h:
+            img[i[1] - min_y][i[0] - min_x] = i[3]
+        else:
+            img[i[1] - min_y][i[0] - min_x] = (50, 50, 50)
+        img_height[i[1] - min_y][i[0] - min_x] = i[2]
+    img = cv2.flip(img, 0)
+    img_height = cv2.flip(img_height, 0)
+    img_copy = copy.deepcopy(img)
+    img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    img_range = cv2.inRange(img_hsv, hsv_min, hsv_max)
+
+    contours, hierarchy = cv2.findContours(img_range, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    h = 0
+    contours_plus = []
+    for cntr in contours:
+        if int(hierarchy[0][h][3]) == -1:
+            mom = cv2.moments(cntr, 1)
+            if int(mom["m00"]) > 100:
+                # cv2.drawContours(img, cntr, -1, (255, 0, 255))
+                contours_plus.append(cntr)
+                # obj = cv2.minAreaRect(cntr)
+                # box = cv2.boxPoints(obj)
+                # box = np.int0(box)
+                # cv2.drawContours(img, [box], -1, (255, 255, 255))  # all contours
+        h += 1
+
+    for cntr in contours_plus:
+        obj = cv2.minAreaRect(cntr)
+        # box = cv2.boxPoints(obj)
+        # box = np.int0(box)
+
+        area = int(obj[1][0] * obj[1][1])
+        if area > 100:
+            # cv2.drawContours(img, [box], -1, (255, 100, 0), 1)  # cur contours
+            # center = (int(obj[0][0]), int(obj[0][1]))
+            color_contour = img[int(cntr[2][0][1])][int(cntr[2][0][0])]
+            if color_contour[2] - 5 > color_contour[1] and color_contour[2] - 5 > color_contour[0]:
+                color_obj = 'red'
+            elif color_contour[0] - 5 > color_contour[1] and color_contour[0] - 5 > color_contour[2]:
+                color_obj = 'blue'
+            else:
+                color_obj = 'none'
+            center_meters[0] = (round(((obj[0][0] + min_x) / 1000), 4))                 # X
+            center_meters[1] = (round(((obj[0][1] + min_y) / 1000), 4))                 # Y
+            center_z = img_height[int(obj[0][1])][int(obj[0][0])]                       # Z
+            long_edge = obj[1][0]
+            if long_edge > 70: lb = True
+            else: lb = False
+            angle = obj[2] * (np.pi / 180.)                                             # RAD
+
+            new_brick = Brick(color_obj, center_meters, center_z, angle, lb)
+            brick_data.append(new_brick)
+            # print(new_brick.color, new_brick.center_xy, new_brick.orientation)
+    old_z = previous_brick.center_z
+    brick_pos = previous_brick.center_xy
+    new_z = img_height[int(brick_pos[1] * 1000 - min_y), int(brick_pos[0] * 1000 - min_x)]
+    dif_z = new_z - old_z
+    return brick_data, dif_z
--- finals/template/test.py
+++ finals/template/test.py
@@ -1,15 +1,15 @@
 from OperateRobot import OperateRobot
 from OperateCamera import OperateCamera
-
+from cv import analyze_image
 
 def main():
-    robot = OperateRobot("172.31.1.25", [0, 0, 0, 0, 0, 0], [[0, 0], [0, 0]], [[0, 0], [0, 0]], [[0, 0], [0, 0]])
+    '''robot = OperateRobot("172.31.1.25", [0, 0, 0, 0, 0, 0], [[0, 0], [0, 0]], [[0, 0], [0, 0]], [[0, 0], [0, 0]])
     cam = OperateCamera()
 
     pos = robot.getl()
     pos[0] += 1
-    robot.movel(pos)
-
+    robot.movel(pos)'''
+    analyze_image()
     '''frame = cam.catch_frame()
     print(frame.points[0])
     print(frame.colors[0])
@@ -17,7 +17,7 @@ def main():
     pcd = cam.open("test.ply")
     cam.visualization_of_points(pcd)'''
 
-    robot.close()
+    # robot.close()
 
 
 if __name__ == "__main__":
