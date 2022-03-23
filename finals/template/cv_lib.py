90018e054c7ef40b7b7640e3c8a33fc87495c1f0
“Sneyk” <stalcraft007@mail.ru>
2022-03-23 23:02:07 +0300

    add cv_lob

---
+++ finals/template/cv_lib.py
@@ -0,0 +1,9 @@
+from typing import Optional
+from OperateCamera import OperateCamera
+from OperateRobot import OperateRobot
+from constants import *
+import open3d as o3d
+import numpy as np
+import copy
+# import math
+import cv2
