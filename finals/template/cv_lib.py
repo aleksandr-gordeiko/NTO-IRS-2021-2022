156d2aa5f840ac549df65afee8543bb23490ce0f
oaleksander <medvedev.asm@outlook.com>
2022-03-23 23:08:57 +0300

    add gap filler

--- finals/template/cv_lib.py
+++ finals/template/cv_lib.py
@@ -1,9 +1,10 @@
-from typing import Optional
-from OperateCamera import OperateCamera
-from OperateRobot import OperateRobot
-from constants import *
-import open3d as o3d
+import cv2 as cv
 import numpy as np
-import copy
-# import math
-import cv2
+
+
+def fill_gaps(mat: cv.mat_wrapper, n_iterations=20) -> cv.mat_wrapper:
+    res = mat.copy()
+    for i in range(n_iterations):
+        cv.copyTo(cv.dilate(res, cv.getStructuringElement(cv.MORPH_ELLIPSE, (3, 3)), iterations=2),
+                  dst=res, mask=cv.inRange(res, np.array([0, 0, 0]), np.array([1, 1, 1])))
+    return res
